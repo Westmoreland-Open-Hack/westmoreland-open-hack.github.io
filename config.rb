b4e05c6e3159c6057073fb2099cdbb6ce936656d
Paul Scarrone <pscarrone@thinkthroughmath.com>
2014-09-14 13:32:37 -0400

    using data and bower'd components to include handlebars

--- config.rb
+++ config.rb
@@ -49,10 +49,6 @@ set :css_dir, 'stylesheets'
 
 set :js_dir, 'javascripts'
 
-set :images_dir, 'images'
-
-set :files_dir, 'files'
-
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
