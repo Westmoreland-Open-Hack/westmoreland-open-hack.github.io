9dc061a314d77be34041f13a170796a7e16c31a9
Paul Scarrone <pscarrone@thinkthroughmath.com>
2014-09-28 12:39:34 -0400

    Bower components should not have been in the JS dir
    This causes build errros instead we will tie into the after config hook and include the bower folder dynamically to config

--- config.rb
+++ config.rb
@@ -49,6 +49,12 @@ set :css_dir, 'stylesheets'
 
 set :js_dir, 'javascripts'
 
+# Add bower's directory to sprockets asset path
+after_configuration do
+  @bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
+  sprockets.append_path File.join "#{root}", @bower_config["directory"]
+end
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
--- source/javascripts/meetup.js
+++ source/javascripts/meetup.js
@@ -2,7 +2,7 @@
   var meetupResults;
 
   $(function(){
-    http://api.meetup.com/2/events?group_id=13038502&sign=true&key=6330357c7f54114c744c5f33cd122f
+    //http://api.meetup.com/2/events?group_id=13038502&sign=true&key=6330357c7f54114c744c5f33cd122f
     $.getJSON( "http://api.meetup.com/2/events?text_format=simplehtml&format=json&page=4&group_id="+meetup.groupId+"&sign=true&key="+meetup.APIKey+"&callback=?", function(data){
       meetupResults = data.results;
     }).done(function(){
@@ -12,6 +12,6 @@
 
   function printResults(){
     $("#events").html(HandlebarsTemplates['events']({"name": "thing", "venue": "My Place", "time": "now"}));
-  };
+  }
 
 }(jQuery));
